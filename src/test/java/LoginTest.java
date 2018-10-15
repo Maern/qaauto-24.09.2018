63c9a54ef99fe6e876d007d8533b14e90eb78b7a
Mykola Avdieiev <ronsaval@gmail.com>
2018-10-15 19:17:06 +0300

    Lesson 4 homework

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,7 +1,12 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static java.lang.Thread.sleep;
 
 public class LoginTest {
     /**
@@ -22,11 +27,20 @@ public class LoginTest {
      */
     @Test
 
-    public void  successfulLoginTest () {
+    public void  successfulLoginTest () throws InterruptedException {
+        String logIn = "avdieievm@gmail.com";
+        String password = "Blastek17";
         WebDriver webDriver = new FirefoxDriver();
         webDriver.navigate().to(  "https://www.linkedin.com");
         //Assert.assertEquals("actual", "expected", "error msg");
         Assert.assertEquals(webDriver.getCurrentUrl(),"https://www.linkedin.com/", "wrong Homepage URL");
+        WebElement loginField = webDriver.findElement(By.id("login-email"));
+        WebElement passwordField = webDriver.findElement(By.id("login-password"));
+        loginField.sendKeys(logIn);
+        passwordField.sendKeys(password);
+        passwordField.sendKeys(Keys.RETURN);
+        sleep (10000);
+        Assert.assertEquals(webDriver.getCurrentUrl(),"https://www.linkedin.com/feed/","Incorrect page is loaded");
 
         webDriver.quit();
     }
